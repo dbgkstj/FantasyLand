d028ebbb64054ea99bfec344b4d8b9dd7e4c754a
dbgkstj <66237418+dbgkstj@users.noreply.github.com>
2020-10-05 15:53:52 +0900

    10/05유한서
    mmm

---
+++ Assets/01.Scenes/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform[] points;
+    void Start()
+    {
+        GameObject wayPointGroup = GameObject.Find("WayPointGroup");
+        if(wayPointGroup != null)
+        {
+            points = wayPointGroup.GetComponentInChildren<Transform>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

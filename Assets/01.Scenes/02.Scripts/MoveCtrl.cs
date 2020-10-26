a994c8fcd0084fea5d6bd440e8fc42f8ccb25764
유한서 <66237418+dbgkstj@users.noreply.github.com>
2020-10-26 16:03:52 +0900

    10/26(30313 유한서)

---
+++ Assets/01.Scenes/02.Scripts/MoveCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    private Transform camTr;
+    private Transform tr;
+    void Start()
+    {
+        //메인 카메라의 Transform 컴포넌트 추출
+        camTr = Camera.main.GetComponent<Transform>();
+        tr = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        //메인 카메라를 향해 Z축 회전
+        tr.LookAt(camTr.position);
+    }
+}
